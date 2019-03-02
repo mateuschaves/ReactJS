fb4c15e241ddca6e1bb1ea5109fea6e4aff59239
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-03-02 16:26:39 -0300

    Implementado a paginação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -7,33 +7,61 @@ export default class Main extends Component {
 
     state = {
         products: [],
+        productInfo: {},
+        page: 1
     }
 
     componentDidMount() {
         this.loadProducts();
     }
 
-    loadProducts = async () => {
-         const response = await api.get('/products');
-         this.setState({ products: response.data.docs });
+    loadProducts = async (page = 1) => {
+         const response = await api.get(`/products?page=${page}`);
+         const { docs, ...productInfo } = response.data;
+         this.setState({ products: docs, productInfo, page});
          //this.state.products = response.data.docs;
     }
 
-    render() {
-        const { products } = this.state;
-        return (
-           <div className="product-list">
-                { products && this.state.products.map( (product, index) => {
-                    return (
-                        <article key={index}>
-                            <strong> { product.title } </strong>
-                            <p> {product.description} </p>
+    prevPage = () => {
+        const { page } = this.state;
 
-                            <a href="#"> Detalhes </a>
-                        </article>
-                    );
-                } )}
-           </div>
+        if(page == 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    nexPage = () => {
+        const { page, productInfo } = this.state;
+
+        if(page == productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
+
+    render() {
+        const { products, page, productInfo } = this.state;
+        return (
+            <div className="product-list">
+                    { products && this.state.products.map( (product, index) => {
+                        return (
+                            <article key={index}>
+                                <strong> { product.title } </strong>
+                                <p> {product.description} </p>
+
+                                <a href="#"> Detalhes </a>
+                            </article>
+                        );
+                    } )}
+
+                <div className="actions">
+                    <button disabled={page == 1} onClick={this.prevPage} > Anterior </button>
+                    <button disabled={page == productInfo.pages} onClick={this.nexPage}> Próximo </button>
+                </div>
+            </div>
         )
     }
 }
